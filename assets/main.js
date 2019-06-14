75ffa71854db00e20a86cedc769d46a2a83806af
David Weid <david.weid.2@gmail.com>
2019-06-13 23:57:29 -0400

    Setting up area for videos

--- assets/main.js
+++ assets/main.js
@@ -4,37 +4,27 @@ const YT_API_KEY = "AIzaSyDyCC_F0psdc0S0fjDC_5qp6xYPsfp99wY";
 const URL =
   "https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyDyCC_F0psdc0S0fjDC_5qp6xYPsfp99wY&part=snippet,player";
 
-const troyeChannel =
-  "https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.channels.list?part=contentDetails&forUsername=Troye+Sivan";
+const user = "TroyeSivan18";
 
-// $.ajax({
-//   url: troyeChannel,
-//   method: "GET"
-// }).then(response => {
-//   const result = response;
-//   console.log(result);
-// });
+const videos = user + "/videos";
 
-// const start = () => {
-//   gapi.client
-//     .init({
-//       apiKey: YT_API_KEY,
-//       discoveryDocs: ["https://people.googleapis.com/$discovery/rest"]
-//     })
-//     .then(() => {
-//       return gapi.client.people.people.get({
-//         resourceName: "people/me",
-//         "requestMask.includeField": "person.names"
-//       });
-//     })
-//     .then(
-//       response => {
-//         console.log(response.result);
-//       },
-//       reason => {
-//         console.log("Error: " + reason.result.error.message);
-//       }
-//     );
-// };
+const singingVideosPlaylistID = "PLCruzMXby65nwW7AcuE8Zh84ZPrEdTFDv";
 
-// gapi.load("client", start);
+const playlistURL = "https://www.googleapis.com/youtube/v3/playlistItems";
+
+const options = {
+  part: "snippet",
+  key: YT_API_KEY,
+  maxResults: 10,
+  playlistId: singingVideosPlaylistID
+};
+
+const loadVids = () => {
+  $.getJSON(playlistURL, options, data => {
+    console.log(data);
+  });
+};
+
+$(document).ready(() => {
+    // loadVids();
+});
