4868a261bd7db0f58be3a013f6be1f8719166b58
David Weid <david.weid.2@gmail.com>
2019-08-21 17:46:00 -0400

    Title bar now shrinks on scroll from top 80px

--- assets/main.js
+++ assets/main.js
@@ -124,6 +124,20 @@ const video = video => {
   }
 };
 
+const scrollTitle = () => {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    $("#hidden-title").css("font-size", "1.3em");
+    $("#title").css("font-size", "1.3em");
+  } else {
+    $("#hidden-title").css("font-size", "2em");
+    $("#title").css("font-size", "2em");
+  }
+};
+
+window.onscroll = function() {
+  scrollTitle();
+};
+
 $(() => {
   loadVids();
 });
