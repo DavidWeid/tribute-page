b8db42f86186d9dacb0cfa34183956f605fad4e0
David Weid <david.weid.2@gmail.com>
2019-08-30 10:25:17 -0400

    Connect to giphy api

--- assets/photos.js
+++ assets/photos.js
@@ -1,5 +1,17 @@
 console.log("Online with Photos!");
 
+const grabGiphs = () => {
+  const key = "g1Can9EJuWv920hp8qsAkS5yv0CX99se";
+
+  fetch(`http://api.giphy.com/v1/gifs/trending?q=troye+sivan&api_key=${key}`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      console.log(myJson);
+    });
+};
+
 const scrollTitle = () => {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     $("#hidden-title").css("font-size", "1.3em");
@@ -8,6 +20,10 @@ const scrollTitle = () => {
     $("#hidden-title").css("font-size", "2em");
     $("#title").css("font-size", "2em");
   }
+};
+
+window.onload = () => {
+  grabGiphs();
 };
 
 window.onscroll = function() {
