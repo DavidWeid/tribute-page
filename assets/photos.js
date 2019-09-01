e1dc17d51e9d8db8e076a142a8be14e09892ae20
David Weid <david.weid.2@gmail.com>
2019-09-01 15:03:13 -0400

    50 gifs load when user scrolls to bottom of page. Could clean up visuals on columns and new rows

--- assets/photos.js
+++ assets/photos.js
@@ -1,11 +1,13 @@
 console.log("Online with Photos!");
 
+let offset = 0;
+
 // Grab Gifs from Giphy
 const grabGiphs = () => {
   const key = "g1Can9EJuWv920hp8qsAkS5yv0CX99se";
 
   fetch(
-    `https://api.giphy.com/v1/gifs/search?q=troye+sivan&limit=50&api_key=${key}`
+    `https://api.giphy.com/v1/gifs/search?q=troye+sivan&limit=50&offset=${offset}&api_key=${key}`
   )
     .then(function(response) {
       return response.json();
@@ -18,18 +20,27 @@ const grabGiphs = () => {
 
 const createGif = gifArray => {
   console.log(gifArray);
+  const container = $("<div>").addClass("masonry");
   const gifItemArray = gifArray.map(gifObj => {
-    const img = $("<img>").addClass("photo-gif")
+    const img = $("<img>")
+      .addClass("photo-gif")
       .attr("src", gifObj.images.original.url)
       .attr("alt", gifObj.title);
-    return img;
+    $(container).append(img);
+    return container;
   });
   console.log(gifItemArray);
-  const target = $(".masonry");
+  const target = $("#main-photos");
   gifItemArray.forEach(item => {
     $(target).append(item);
   });
 };
+
+// $("#more-gifs").on("click", function() {
+//   console.log("More Clicked");
+//   offset += 50;
+//   grabGiphs();
+// });
 
 // Watch for scroll (for title-bar visuals)
 const scrollTitle = () => {
@@ -49,3 +60,17 @@ window.onload = () => {
 window.onscroll = function() {
   scrollTitle();
 };
+
+$(window).on(
+  "scroll",
+  _.throttle(function() {
+    if (
+      $(window).scrollTop() + $(window).height() >
+      $(document).height() - 100
+    ) {
+      console.log("near bottom!");
+      offset += 50;
+      grabGiphs();
+    }
+  }, 1500)
+);
